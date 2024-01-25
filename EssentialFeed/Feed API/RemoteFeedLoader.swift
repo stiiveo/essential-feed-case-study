ac94e5d29b353239eb5791ead98a59baf64722d3
Jason Ou <jason.ou.pro@gmail.com>
2024-01-25 17:41:39 +0800

    Move "RemoteFeedLoader" components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by JasonOu on 2024/1/25.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
