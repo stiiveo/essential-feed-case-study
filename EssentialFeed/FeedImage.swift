a8d722757faff4cc4b640612c026b3ee37e58967
Jason Ou <jason.ou.pro@gmail.com>
2024-01-25 15:30:43 +0800

    Create the FeedImage model

---
+++ EssentialFeed/FeedImage.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImage.swift
+//  EssentialFeed
+//
+//  Created by JasonOu on 2024/1/25.
+//
+
+import Foundation
+
+struct FeedImage {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
