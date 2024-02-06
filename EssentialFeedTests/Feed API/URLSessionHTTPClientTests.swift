3231ffab55b1fd1fa165235eb84c0c5a220e8325
Jason Ou <jason.ou.pro@gmail.com>
2024-02-06 16:00:25 +0800

    Resumes URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,7 +10,7 @@ final class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url, completionHandler: { _, _, _ in })
+        session.dataTask(with: url, completionHandler: { _, _, _ in }).resume()
     }
 }
 
@@ -26,17 +26,44 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedUrls, [url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private final class URLSessionSpy: URLSession {
         private(set) var receivedUrls = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedUrls.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
     
-    private final class FakeURLSessionDataTask: URLSessionDataTask {}
+    private final class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {}
+    }
     
+    private final class URLSessionDataTaskSpy: URLSessionDataTask {
+        private(set) var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
 }
